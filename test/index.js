64532f0e3fc863fd0686b6e0d8891d573ac41844
Johnny Hauser <johnnyhauser@gmail.com>
2016-06-09 19:13:46 -0500

    renaming

--- test/index.js
+++ test/index.js
@@ -1,8 +1,7 @@
 var test = require('tape')
-var whenCnctReady = require('../')
+var tryConnect = require('../')
 var http = require('http')
 var testPort = 9999
-var Promise = require('bluebird')
 
 function Server() {
   return http.createServer(function(req, res) { res.end() })
@@ -12,7 +11,7 @@ test('emits "connected" when connected', function(t) {
   t.plan(1)
   var server = Server()
   server.listen(testPort, function() {
-    whenCnctReady({port: testPort, timeout: 1000}).on('connected', function() {
+    tryConnect({port: testPort, timeout: 1000}).on('connected', function() {
       server.close(function() {
         t.pass()
       })
@@ -22,12 +21,12 @@ test('emits "connected" when connected', function(t) {
 
 test('emits "timeout" when timed out', function(t) {
   t.plan(1)
-  whenCnctReady({port: testPort, retry: 40, timeout: 100}).on('timeout', t.pass)
+  tryConnect({port: testPort, retry: 40, timeout: 100}).on('timeout', t.pass)
 })
 
 test('emits "retry" with connection error message', function(t) {
   t.plan(3)
-  whenCnctReady({port: testPort, retry: 10, timeout: 30})
+  tryConnect({port: testPort, retry: 10, timeout: 30})
   .on('retry', function(reason) {
     t.true(reason)
   })
